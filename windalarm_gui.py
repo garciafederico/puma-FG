d79370b917e7b456f6b6b7e2cc203d3759ecffb3
Federico García <39309065+garciafederico@users.noreply.github.com>
2019-05-06 16:22:17 +0200

    Recommended update for windalarm.

--- windalarm_gui.py
+++ windalarm_gui.py
@@ -18,9 +18,8 @@ CRED = '\033[91m'
 CEND = '\033[0m'
 
 sound=True
-refresh_every_sec=15
-timezero=time.time()
-firsttime=True
+refresh_every_sec=20
+alarm_trigger=25
 
 if(os.system('which lynx > /dev/null')!=0):
 		print('You must first install lynx with apt-get.')
@@ -40,7 +39,6 @@ def close_window():
 def sigint_handler(signum, frame):
 	print('\n\n Stopped by user with CTRL+C')
 	exit(0)
-
 signal.signal(signal.SIGINT, sigint_handler)
 
 
@@ -51,39 +49,58 @@ def get_weather():
 print('Welcome to IAR wind service (press Ctrl+C to exit)')
 
 window = Tk()
-window.title("IAR wind service")
-window.geometry('250x40')
+window.title("IAR wind PuMA")
+window.configure(bg="black")
+#window.geometry('250x40')
 window.protocol("WM_DELETE_WINDOW", close_window)
+
+
+lines=get_weather()
+wind  = lines.split('\n')[8]
+value = int(wind.split()[2])
+
+if value>alarm_trigger:
+	print(CRED+'\r{0}   -   ATTENTION HIGH WIND!!!            '.format(wind)+CEND,end='')
+	lbl = Label(window, text="{0}   -   HIGH WIND!!!".format(wind), bg = "red", fg="white")
+	if(sound): os.system('spd-say "ATTENTION PLEASE: WIND IS ABOVE 25 KILOMETERS PER HOUR"')
+else:
+	print('\r{0}   -   WIND IS FINE                      '.format(wind),end='')
+	lbl = Label(window, text="{0}   -   WIND IS FINE   ".format(wind), fg="white", bg="black")
+
+lbl.grid(column=0, row=0)
+
+style = ttk.Style()
+style.theme_use('default')
+style.configure("black.Horizontal.TProgressbar", background='black')
+bar = Progressbar(window, length=200, style='black.Horizontal.TProgressbar')
+bar['value'] = 0
+bar.grid(column=0, row=1)
+
+timezero=time.time()
 
 running=True
 
 while running:
-    interval=time.time()-timezero
+	interval=time.time()-timezero
 
-    if((interval > refresh_every_sec) | firsttime):
-        firsttime=False
-        timezero+=interval
-        lines=get_weather()
-        wind  = lines.split('\n')[8]
-        value = int(wind.split()[2])
+	if(interval > refresh_every_sec):
+		timezero+=interval
+		lines=get_weather()
+		wind  = lines.split('\n')[8]
+		value = int(wind.split()[2])
 
-        if value>25:
-	        print(CRED+'\r{0}   -   ATTENTION HIGH WIND!!!            '.format(wind)+CEND,end='')
-	        lbl = Label(window, text="{0}   -   HIGH WIND!!!".format(wind), bg = "red", fg="white")
-	        if(sound): os.system('spd-say "ATTENTION PLEASE: WIND IS ABOVE 25 KILOMETERS PER HOUR"')
-        else:
-	        print('\r{0}   -   WIND IS FINE                      '.format(wind),end='')
-	        lbl = Label(window, text="{0}   -   WIND IS FINE   ".format(wind), fg="green", bg=None)
-	        
-        lbl.grid(column=0, row=0)
-	    
-    style = ttk.Style()
-    style.theme_use('default')
-    style.configure("black.Horizontal.TProgressbar", background='black')
-    bar = Progressbar(window, length=200, style='black.Horizontal.TProgressbar')
-    bar['value'] = float(interval*100.)/refresh_every_sec
-    bar.grid(column=0, row=1)
-
-    time.sleep(1.0)
-    
-    window.update()
+		if value>alarm_trigger:
+			print(CRED+'\r{0}   -   ATTENTION HIGH WIND!!!            '.format(wind)+CEND,end='')
+			lbl['text']="{0}   -   HIGH WIND!!!".format(wind)
+			lbl['fg']="white"
+			lbl['bg']="red"
+			if(sound): os.system('spd-say "ATTENTION PLEASE: WIND IS ABOVE 25 KILOMETERS PER HOUR"')
+		else:
+			print('\r{0}   -   WIND IS FINE                      '.format(wind),end='')
+			lbl['text']="{0}   -   WIND IS FINE   ".format(wind)
+			lbl['fg']="white"
+			lbl['bg']="black"
+			
+	bar['value'] = float(interval*100.)/refresh_every_sec
+	time.sleep(1.0)
+	window.update()
